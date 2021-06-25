ba5513a1e754a0252c7c756dfc6c2d4416ba0fc8
Demi Ogun <demiogun1@gmail.com>
2021-06-25 09:27:09 +0100

    Extract iframe out into Preview component

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,14 @@ import * as esbuild from 'esbuild-wasm'
 import { useState, useEffect, useRef } from 'react'
 import ReactDOM from 'react-dom'
 import CodeEditor from './components/code-editor'
+import Preview from './components/preview'
 import { fetchPlugin } from './plugins/fetch-plugin'
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
 
 const App = () => {
   const ref = useRef<any>()
-  const iframe = useRef<any>()
   const [input, setInput] = useState('')
+  const [code, setCode] = useState('')
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -27,8 +28,6 @@ const App = () => {
       return
     }
 
-    iframe.current.srcdoc = html
-
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -40,30 +39,8 @@ const App = () => {
       },
     })
 
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*')
+    setCode(result.outputFiles[0].text)
   }
-
-  const html = `
-    <html>
-      <head></head>
-      <body>
-        <div id="root"></div>
-        <script>
-          window.addEventListener('message', (event) => {
-            try {
-              eval(event.data)
-            } catch (err) {
-              const root = document.querySelector('#root');
-              root.innerHTML = '<div style="color: red;" ><h4>Runtime Error</h4>' + err + '</div>';
-              console.error(err);
-            }
-          }, false);
-
-
-          </script>
-      </body>
-    </html>
-  `
 
   return (
     <div>
@@ -71,19 +48,10 @@ const App = () => {
         initialValue={'const a = 1;'}
         onChange={(value) => setInput(value)}
       />
-      <textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
       <div>
         <button onClick={onClick}>submit</button>
       </div>
-      <iframe
-        title="mainFrame"
-        sandbox="allow-scripts"
-        srcDoc={html}
-        ref={iframe}
-      />
+      <Preview code={code} />
     </div>
   )
 }
